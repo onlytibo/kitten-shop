433142f55b59162a1e552c579ab63b61f3782912
thibautgallice <gt.thibaut@gmail.com>
2020-03-09 12:58:23 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 
 2.times do 
   User.create(email: 'bojuaugustin@gmail.com', name: "Augustin")
