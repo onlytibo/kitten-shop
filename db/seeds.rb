f75a35402c4cbfab501ade3d1c7f6785203496a9
thibautgallice <gt.thibaut@gmail.com>
2020-03-09 12:38:46 +0100

    setup mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+2.times do 
+  User.create(email: 'bojuaugustin@gmail.com', name: "Augustin")
+end
