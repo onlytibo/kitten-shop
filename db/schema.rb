612019d1cb87b0607d3c90f324b77d190c935cf5
augustinboju <bojuaugustin@gmail.com>
2020-03-09 16:01:18 +0100

    signin et sign up form ok for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_135108) do
+ActiveRecord::Schema.define(version: 2020_03_09_135240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
     t.decimal "price"
