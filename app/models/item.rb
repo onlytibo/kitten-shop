f0d9c7d9e5019c74bfa4052dfdb862ca4f53ff85
thibautgallice <gt.thibaut@gmail.com>
2020-03-11 02:00:24 +0100

    cart done

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,13 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  has_many :cart_items
+  has_many :cart_items, -> { order "title asc" }
   has_many :carts, through: :cart_items
 
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'items#index'
 
   resources :items
   resources :users, only: [:show, :new, :create, :edit, :update], path: 'utilisateurs' do
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     resources :orders, only: [:new, :show, :create], path: 'commandes'
     resources :avatars, only: [:create]
   end
-  resources :carts
-  resources :cart_items
+  resources :cart_items, only: [:create, :destroy]
 end
