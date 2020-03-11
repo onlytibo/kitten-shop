5a86745513af777fb4c93502d59d41c23b9105a2
thibautgallice <gt.thibaut@gmail.com>
2020-03-11 17:53:10 +0100

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -72,12 +72,10 @@ ActiveRecord::Schema.define(version: 2020_03_11_114505) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "cart_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "total"
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
