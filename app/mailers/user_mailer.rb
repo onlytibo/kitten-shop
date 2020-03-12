341846a9dcf29c54ec9a4faefc8d5b6ec0c575b9
thibautgallice <gt.thibaut@gmail.com>
2020-03-13 00:44:36 +0100

    mail order ok

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,24 @@
 class UserMailer < ApplicationMailer
+
   default from: 'no-reply@kitten-shop-thp.herokuapp.com'
 
 
-  def order_email(order)
+  def order_email(order, user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @order = order
+    @user = user
     @order_items = OrderItem.where(order_id:@order.id)
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login'
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: order.user.email, subject: "Your order N°#{@order.id}, Kitten shop ")
   end
 
-  def admin_email(order)
+  def admin_email(order, user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @order = order
+        @user = user
+
     @order_items = OrderItem.where(order_id: @order.id)
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login'
