381c346e6dade6709ee90a80899a2f3f1d4c1a72
augustinboju <bojuaugustin@gmail.com>
2020-03-12 15:29:19 +0100

    update seed

--- db/schema.rb
+++ db/schema.rb
@@ -73,12 +73,10 @@ ActiveRecord::Schema.define(version: 2020_03_12_104958) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "cart_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "total"
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
